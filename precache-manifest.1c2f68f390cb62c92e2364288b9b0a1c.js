eda97cb33fc615a195273536de45c360992ea274
tamhgdc <108126724+tamhgdc@users.noreply.github.com>
2022-08-25 11:03:37 +0700

    Add files via upload

---
+++ precache-manifest.1c2f68f390cb62c92e2364288b9b0a1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c80d2d554eb5f26995e1b7dbf553b26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbc780b4ebb1c21528e4",
+    "url": "/static/css/2.cdf13198.chunk.css"
+  },
+  {
+    "revision": "fd3dadc8a94a50a24442",
+    "url": "/static/css/main.1cab7cda.chunk.css"
+  },
+  {
+    "revision": "cbc780b4ebb1c21528e4",
+    "url": "/static/js/2.7cc436ce.chunk.js"
+  },
+  {
+    "revision": "6b83a8a8c872cd7011d9d896ee5f1e3d",
+    "url": "/static/js/2.7cc436ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd3dadc8a94a50a24442",
+    "url": "/static/js/main.cc330f38.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "1d8aaf85628ef849d82c017e46f26ee5",
+    "url": "/static/media/Girl.1d8aaf85.jpg"
+  }
+]);
